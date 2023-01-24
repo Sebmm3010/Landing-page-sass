aab5015e7608be5578dcc7fd281d747fc7f257fe
Sebastian <rockmadero@gmail.com>
2023-01-24 17:04:00 -0500

    routes creada

---
+++ src/data/data.ts
@@ -0,0 +1,21 @@
+import { Route } from "../interfaces"
+
+
+export const routes: Route[] = [
+    {
+        name: 'Inicio',
+        path: '#'
+    },
+    {
+        name: 'Como funciona?',
+        path: '#funciona'
+    },
+    {
+        name: 'Sobre nosotros',
+        path: '#about'
+    },
+    {
+        name: 'Contactanos',
+        path: '#contact'
+    }
+];
