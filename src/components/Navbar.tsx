93de5435dc7e4a22b248494ab62990b350508bd1
Sebastian <rockmadero@gmail.com>
2023-01-24 17:04:16 -0500

    navabar creada

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import '../styles/navbar.scss';
+
+export const Navbar = () => {
+  return (
+    <header className='navbar'>
+      <div className='navbar__logo'>
+        <p>Logo</p>
+      </div>
+      <ul className='navbar__routes'>
+
+      </ul>
+      <div className="navbar__buttons"></div>
+    </header>
+  )
+}
