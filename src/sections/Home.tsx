048c73e8595d58054af33ef531871e857ed4f36a
Sebastian <rockmadero@gmail.com>
2023-01-26 15:10:24 -0500

    dashborad implementada

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,16 +1,20 @@
+import { Dashboard } from '../components';
 import '../styles/home.scss';
 
 export const Home = () => {
   return (
-    <section id="home" className="home">
+    <>
+      <section id="home" className="home">
         <div className="home__text">
-              <h1>Dale Más Poder</h1>
-              <h1> a Tu Negocio</h1>
+          <h1>Dale Más Poder</h1>
+          <h1> a Tu Negocio</h1>
         </div>
         <div className="input__container">
-            <input type="email" placeholder='Email' />
-            <button>Prueba gratis</button>
+          <input type="email" placeholder='Email' />
+          <button>Prueba gratis</button>
         </div>
-    </section>
+      <Dashboard/>
+      </section>
+    </>
   )
 }
