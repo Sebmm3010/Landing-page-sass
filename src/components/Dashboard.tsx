6f521a7fe2feaa7a32d691c94d5e0f1583a21a82
Sebastian <rockmadero@gmail.com>
2023-01-26 15:10:48 -0500

    dashboard creada

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { dashboard } from '../assets';
+import '../styles/dashboard.scss';
+
+export const Dashboard = () => {
+  return (
+    <div className='dashboard'>
+        <img src={dashboard} alt="dashboard.png" />
+    </div>
+  )
+}
